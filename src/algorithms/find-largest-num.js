f2c902726731b62779df86887e29321e043b26e0
Brad Mallett <bmallett2@gmail.com>
2020-04-03 18:39:24 -0500

    Deleting console log

--- src/algorithms/find-largest-num.js
+++ src/algorithms/find-largest-num.js
@@ -18,8 +18,6 @@ function largestOfFour(arr) {
 const myAnswer = 
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
 
-console.log(myAnswer);
-
 //RETURNS: [25, 48, 21, -3]
 
 module.exports = largestOfFour;
